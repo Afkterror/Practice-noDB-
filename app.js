e5f385ab07df5333953d7be68774c6cba4fa9678
Nick Faradey <afkterror@gmail.com>
2021-04-19 14:00:13 +0700

    basic logic for lists and basic styles

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const _ = require("lodash");
 const app = express();
 const port = 3000;
 
+const listOfLists = [];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,8 +15,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("index", {listOfLists: listOfLists});
 })
+
+app.post("/", function(req, res) {
+  let newList = req.body.newList;
+
+  listOfLists.push(newList);
+
+  res.redirect("/");
+});
 
 app.listen(port, function(req, res) {
   console.log("Server started on port " + port);
